19234f22d5be6502590e3dc1d8a304551a165541
lemon-sazu <lemon.sazu@gmail.com>
2019-10-10 14:34:55 -0400

    Updates

---
+++ precache-manifest.4ec3af40bee010697e8595a8762024bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d644af7325101e1166dafe54970bf554",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "839bca23748cf5e9daf5",
+    "url": "/monsters/static/css/main.a73e2e8a.chunk.css"
+  },
+  {
+    "revision": "c03d3f64331e577d1529",
+    "url": "/monsters/static/js/2.b04a52b1.chunk.js"
+  },
+  {
+    "revision": "839bca23748cf5e9daf5",
+    "url": "/monsters/static/js/main.9deda576.chunk.js"
+  },
+  {
+    "revision": "a2031f3781cfb05103af",
+    "url": "/monsters/static/js/runtime~main.3b5f3fa4.js"
+  }
+]);
